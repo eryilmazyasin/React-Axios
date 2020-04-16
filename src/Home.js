03c09b7deaeee903a10811350d77ecb25294f5fc
Yasin Eryılmaz <yasineryilmazfb@gmail.com>
2020-04-16 21:50:51 +0300

    test

--- src/Home.js
+++ src/Home.js
@@ -16,8 +16,8 @@ class Home extends Component {
             })
         })
         .catch(error => {
-            console.log(error);
-        })
+            console.log(error); 
+        }) 
     }
 
 
